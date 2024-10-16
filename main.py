4700e81f5d4c1ff9f3ff0afa7ae86863aef2ac26
FREDYK1 <fkwekukankam@gmail.com>
2024-10-16 23:22:53 +0000

    Add menubar containing file and help with an action each

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QApplication, QMainWindow
 import sys
 
@@ -5,10 +6,20 @@ import sys
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
         # Set the window title
         self.setWindowTitle("Student Management System")
 
+        # Set Menubar
+        file_menu_item = self.menuBar().addMenu("File")
+        help_menu_item = self.menuBar().addMenu("Help")
 
+        # Set Menubar Actions
+        student_action = QAction("Add Student", self)
+        file_menu_item.addAction(student_action)
+
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
 
 
 
